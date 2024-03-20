e5bd21357e4565e618212d7069dc2926e424fe13
Ajith <ajithkannan13698@gmail.com>
2024-03-20 21:01:21 +0530

    Prepare basemodel completed

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -9,3 +9,15 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+@dataclass(frozen=True) # It doesn't allow any other data than the mentioned below
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+
